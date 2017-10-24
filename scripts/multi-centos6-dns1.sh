9f118ebb86d64b04a38ad6261a24f4e8e5df0a64
Lee Wilkinson <lee.vvilkinson@gmail.com>
2017-10-24 18:17:47 +0100

    Null'd dns1&2, pre setup yum detail

--- scripts/multi-centos6-dns1.sh
+++ scripts/multi-centos6-dns1.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# DNS 1
-yum install -y mysql mysql-server mysql-devel
-chkconfig --add mysqld
-chkconfig mysqld on
+# DNS 1  Master
 
-service mysqld start
 
-mysql -u root -e "SHOW DATABASES"
 
 # END
--- scripts/multi-centos6-dns2.sh
+++ scripts/multi-centos6-dns2.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# DNS 2
-yum install -y mysql mysql-server mysql-devel
-chkconfig --add mysqld
-chkconfig mysqld on
+# DNS 2  Slave
 
-service mysqld start
 
-mysql -u root -e "SHOW DATABASES"
 
 # END
